68bab6c9f831d48e780e201abcf7113063f4e285
Alexander-Page <51935845+Alexander-Page@users.noreply.github.com>
2020-02-25 19:24:57 -0600

    fixed format

--- test.c
+++ test.c
@@ -5,12 +5,15 @@
 int main()
 {
    struct c_vector vec = vec_initialize(3);
+   
    push(&vec, 3);
    push(&vec, 14);
    push(&vec, 24);
    push(&vec, 13);
    push(&vec, 103);
+
    printvec(vec);
    vec_reverse(&vec);
+   printf("\nVector reversed:\n");
    printvec(vec);
 }
